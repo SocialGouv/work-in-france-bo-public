b62ae2a7ed037c42496b00df67c58aacf4a64af4
kemar <marc@marcarea.com>
2018-04-19 15:50:04 +0200

    Handle Cross-Origin Resource Sharing for koa

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
-const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const cors = require('@koa/cors')
+const Koa = require('koa')
 
 const apiRoutes = require('./routes/api')
 
 const app = new Koa()
 const PORT = process.env.PORT || 1337
 
+// https://github.com/koajs/cors
+app.use(cors())
 app.use(bodyParser())
 
 // Routes.
