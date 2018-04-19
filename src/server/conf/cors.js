da4fc961e50e270be858b70e4023ab11bebfa369
kemar <marc@marcarea.com>
2018-04-19 16:46:22 +0200

    Allow CORS requests for multiple origins

---
+++ src/server/conf/cors.js
@@ -0,0 +1,27 @@
+// https://github.com/koajs/cors
+// https://github.com/koajs/cors/issues/12
+
+const validOrigins = [
+  'http://localhost:8080',
+  'http://192.168.1.11:8080',
+]
+
+function originIsValid (origin) {
+  return validOrigins.indexOf(origin) !== -1
+}
+
+function verifyOrigin (ctx) {
+  const origin = ctx.headers.origin
+  if (!originIsValid(origin)) {
+    return false
+  }
+  return origin
+}
+
+const config = {
+  origin: verifyOrigin,
+}
+
+module.exports = {
+  config,
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,15 @@
 const bodyParser = require('koa-bodyparser')
-const cors = require('@koa/cors')
 const Koa = require('koa')
+const cors = require('@koa/cors')
 
+const corsConf = require('./conf/cors')
 const apiRoutes = require('./routes/api')
 
 const app = new Koa()
 const PORT = process.env.PORT || 1337
 
-// https://github.com/koajs/cors
-app.use(cors())
+app.use(cors(corsConf.config))
 app.use(bodyParser())
-
-// Routes.
 app.use(apiRoutes.routes())
 
 // Server.
